22a1e227924417f9ab99e209c3c701f8970db827
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-08-23 22:52:23 -0500

    added code and found error
    found issue in index.html, script src. changed to sub folder scripts,js

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,30 @@ function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = $('<li></li>');
-   let inputValue = $('#input').val();
-   li.append(document.createTextNode(inputValue));
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(document.createTextNode(inputValue));
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     $('#list').appendChild(li);
-   }
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+      $('#list').append(li);
+  }
 
  //2. Crossing out an item from the list of items:
-
-    li.on('dblclick', function crossOut() {
+  li.on('dblclick', function crossOut() {
       li.addClass('strike');
-    })
- 	   li.addEventListener("dblclick",crossOut);
+  });
+
 
  //3(i). Adding the delete button "X":
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
+   let crossOutButton = $('<crossOutButton> x </crossOutButton>');
+   li.append(crossOutButton);
+//3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+ 	crossOutButton.on('click', function deleteListItem() {
+      li.addClass('delete');
+    });
 
- 	crossOutButton.addEventListener("click", deleteListItem);
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
- 	}
  // 4. Reordering the items:
    $('#list').sortable();
-
 }
